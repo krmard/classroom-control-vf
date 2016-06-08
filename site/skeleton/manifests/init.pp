ba79b69b4decdc6d3c8c062712973ae6e1c99dde
Danijel Krmar <krmard@gmail.com>
2016-06-08 13:06:37 +0200

    Exercise 9.3 Manage file resources

---
+++ site/skeleton/manifests/init.pp
@@ -0,0 +1,17 @@
+class skeleton {
+
+  file { '/etc/skel':
+    ensure  => directory,
+    mode    => '0755',
+    owner   => 'root',
+    group   => 'root',
+  }
+  
+  file { '/etc/skel/.bashrc':
+    ensure  => file,
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    source  => 'puppet:///modules/skeleton/bashrc'
+  }
+}
