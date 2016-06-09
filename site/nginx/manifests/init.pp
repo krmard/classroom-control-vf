f2a96a256ebe037d970910eb21251117e9a0ceda
Danijel Krmar <krmard@gmail.com>
2016-06-09 11:01:16 +0200

    Excersie 13.1 Code simplification

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,42 +1,29 @@
 class nginx {
+  File {
+    owner => 'root',
+    group => 'root',
+    mode  => '0644',
+  }
   package { 'nginx':
     ensure  => present,
   }
+  file { [ '/etc/nginx/conf.d', '/var/www/' ]:
+    ensure  => directory,
+  }
   file { '/etc/nginx/nginx.conf':
     ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
     source  => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
     notify  => Service['nginx'],
   }
-  file { '/etc/nginx/conf.d':
-    ensure  => directory,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0775',
-  }
   file { '/etc/nginx/conf.d/default.conf':
     ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
     source  => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
     notify  => Service['nginx'],
   }
-  file { '/var/www/':
-    ensure  => directory,
-    owner   => 'nginx',
-    group   => 'nginx',
-    mode    => '0775',
-  }
   file { '/var/www/index.html':
     ensure  => file,
-    owner   => 'nginx',
-    group   => 'nginx',
-    mode    => '0644',
     source  => 'puppet:///modules/nginx/index.html',
   }
   service { 'nginx':
